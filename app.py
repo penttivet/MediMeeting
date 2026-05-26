c72bdd761dd99b179ec6f46d0ba10ec39174c9b5
penttivet <67260026+penttivet@users.noreply.github.com>
2026-05-26 17:54:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -772,6 +772,10 @@ def ensure_admin():
 with app.app_context():
     ensure_admin()
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
+
+
+
